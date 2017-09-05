e3ee2ef5a1aa3e6b89ffe495d95ce540b204c4cf
Björn <bjoeh523@student.liu.se>
2017-09-05 19:24:21 +0200

    divided code into smaller functions and added some comments

--- lab2/evilhangman/src/evilhangman.cpp
+++ lab2/evilhangman/src/evilhangman.cpp
@@ -10,14 +10,19 @@ const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 const string path = "/Users/bjorneman/Desktop/TDDD86/labb2/evilhangman/res/di.txt";
 const char UNREVIELED = '_';
 
+string getLargestGroupKey(map<a, vector<B> m);
 string getFamily(string w, string curFam, char guess);
+template <typename T>
+T askUser(String message);
 
-int main() {
+int main()
+{
     cout << "Welcome to Hangman." << endl;
+
+    // Load the words into a dictionary where the words are categorized by length
     map<int, vector<string>> dict;
     string input;
     ifstream file(path);
-
     while(getline(file, input)) {
         vector<string>& vec = dict[input.size()];
         vec.push_back(input);
@@ -25,21 +30,17 @@ int main() {
 
     int len = 0;
     while (len == 0 || dict.count(len) < 1){
-        cout << "wordlength: ";
-        cin >> len;
+        len = askUser("Wordlength: ");
     }
 
     int guesses = 0;
     while (guesses < 1){
-        cout << "guesses: ";
-        cin >> guesses;
+        guesses = askUser("Guess amount: ");
     }
 
     bool see_remaining;
     while(true) {
-        char ans;
-        cout << "See remaining words? [y/n]: ";
-        cin >> ans;
+        char ans = askUser("Do you want to see the amount of remaining words?[y/n]: ");
         if (ans == 'y') {
             see_remaining = true;
             break;
@@ -50,55 +51,61 @@ int main() {
         }
     }
 
-    vector<string>& words = dict[len];
+    // The vector containing the words that matches the length.
+    vector<string>& remainingWords = dict[len];
+    // The current "word-family" is the family containing all defined words of given length;
     string family(len, UNREVIELED);
 
     while(true){
-        for (auto& w : words) {
-            cout << w << ", ";
-        }
-        cout << endl;
-
         cout << "Guesses left: " << guesses << endl
              << family << endl;
+
         if (see_remaining)
-            cout << "remaining words: " << words.size() << endl;
+            cout << "remaining words: " << remainingWords.size() << endl;
 
-        char guess;
-        cout << "guess: ";
-        cin >> guess;
+        char guess = askUser("Guess: ");
 
+        // Place all remaining words into "word-families" based on the current guesses.
         map<string, vector<string>> families;
-        for (auto& w : words) {
+        for (auto& w : remainingWords) {
             vector<string>& fam = families[getFamily(w, family, guess)];
             fam.push_back(w);
         }
 
         string prev_fam = family;
-        int largest_fam_size = 0;
-        for (auto& fam : families) {
-            if (fam.second.size() > largest_fam_size) {
-                family = fam.first;
-                cout << fam.first << endl;
-                largest_fam_size = fam.second.size();
-            }
-        }
+        // Find the largest "word-family" and store its words.
+        family = getLargestGroupKey(families);
+        remainingWords = families[family];
 
-        words = families[family];
-
+        // If the word is finished, the player has won.
         if(family.find(UNREVIELED) == string::npos){
             cout << "Grattis, du vann!" << endl;
             return 0;
         }
 
+        // If the family did not change, the user has given a wrong guess.
         if (family == prev_fam) {
             guesses -= 1;
         }
 
-
     }
 
     return 0;
+}
+
+template<typename A>
+template<typename B>
+string getLargestGroupKey(map<a, vector<B> m) {
+    int largest_group_size = 0;
+    A key;
+    for (auto& group : m) {
+        if (group.second.size() > largest_group_size) {
+            key = group.first;
+            largest_group_size = group.second.size();
+        }
+    }
+
+    return key;
 }
 
 string getFamily(string w, string curFam, char guess) {
@@ -111,5 +118,13 @@ string getFamily(string w, string curFam, char guess) {
 
     return res;
 
+}
+
+template <typename T>
+T askUser(string msg) {
+    T ans;
+    cout << msg;
+    cin >> ans;
+    return ans;
 }
 
